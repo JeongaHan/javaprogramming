1a78e513a4b109a52613093c4373ab1981cb7d2f
JeongaHan <44192694+JeongaHan@users.noreply.github.com>
2020-08-16 23:46:10 +0900

    Create is_square_root.java
    // I don't understand why this code is wrong.
    
    import java.util.*;
    class Solution {
        public long solution(long n) {
            long answer = 0;
            long sq = (long) Math.sqrt(n);
            for(int i = 1; i <= sq; i++) {
            	if(i*i==n) {
            		return (long)(i+1)*(i+1);
            	}
            }
            return -1;
        }
    }

---
+++ programmers/is_square_root.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Solution {
+    public long solution(long n) {
+        long sq = (long) Math.sqrt(n);
+        long po = (long) Math.pow(sq, 2);
+        return (long) (po==n?Math.pow(sq+1, 2):-1);
+    }
+}
