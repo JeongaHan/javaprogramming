5427f384687f726e3d0d2e6c6330b80963206574
JeongaHan <44192694+JeongaHan@users.noreply.github.com>
2020-08-17 10:25:39 +0900

    Create integer_inverted_array.java

---
+++ programmers/integer_inverted_array.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public static int[] solution(long n) {
+    	List<Integer> list = new ArrayList<Integer>();
+    	while(n>0) {
+    		list.add((int) (n%10));
+    		n/=10;
+    	}
+    	int [] answer = new int[list.size()];
+    	for(int i = 0; i < answer.length; i++) {
+    		answer[i] = list.get(i);
+    	}
+        return answer;
+    }
+}
