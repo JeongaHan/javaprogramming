d836aa48f44be0d34b6e25a5423c0dd23498f3da
JeongaHan <44192694+JeongaHan@users.noreply.github.com>
2020-08-23 19:53:26 +0900

    Update pe_clothes.java
    acc 83.3

--- programmers/pe_clothes.java
+++ programmers/pe_clothes.java
@@ -4,14 +4,28 @@ class Solution {
         for(int i = 0; i < reserve.length; i++) {
         	for(int j = 0; j < lost.length; j++) {
         		if(lost[j]==reserve[i]) {
+        			if(answer >= n)
+    					break;
+        			System.out.println(reserve[i] + "가 " + lost[j]);
         			reserve[i] = 0;
         			answer+=1;
-                    break;
-        		}else if(lost[j]+1!=reserve[i]) {
-        			if(lost[j]==reserve[i]-1||lost[j]==reserve[i]+1) {
+        			break;
+        		}else {
+        			if(lost[j]==reserve[i]-1) {
+        				if(answer >= n)
+        					break;
+        				System.out.println(reserve[i] + "가 " + lost[j]);
         				reserve[i] = 0;
         				answer+=1;
-                        break;
+        				break;
+        			}
+        			else if(lost[j]==reserve[i]+1) {
+        				if(answer >= n)
+        					break;
+        				System.out.println(reserve[i] + "가 " + lost[j]);
+        				reserve[i] = 0;
+        				answer+=1;
+        				break;
         			}
         		}
         	}
