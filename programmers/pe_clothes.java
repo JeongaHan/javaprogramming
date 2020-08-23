a91db98b86f771e72916ad6855dd985ebc71a097
JeongaHan <44192694+JeongaHan@users.noreply.github.com>
2020-08-23 20:27:52 +0900

    Update pe_clothes.java
    completed!!!!

--- programmers/pe_clothes.java
+++ programmers/pe_clothes.java
@@ -3,32 +3,30 @@ class Solution {
         int answer = n - lost.length;
         for(int i = 0; i < reserve.length; i++) {
         	for(int j = 0; j < lost.length; j++) {
+        		System.out.println("reserve : " + i + " , lost : " + j);
         		if(lost[j]==reserve[i]) {
         			if(answer >= n)
     					break;
-        			System.out.println(reserve[i] + "가 " + lost[j]);
-        			reserve[i] = 0;
+        			System.out.println(reserve[i] + "가 " + lost[j] + " *");
+        			reserve[i] = -1;
+        			lost[j]=-1;
         			answer+=1;
-        			break;
-        		}else {
-        			if(lost[j]==reserve[i]-1) {
-        				if(answer >= n)
-        					break;
-        				System.out.println(reserve[i] + "가 " + lost[j]);
-        				reserve[i] = 0;
-        				answer+=1;
-        				break;
-        			}
-        			else if(lost[j]==reserve[i]+1) {
-        				if(answer >= n)
-        					break;
-        				System.out.println(reserve[i] + "가 " + lost[j]);
-        				reserve[i] = 0;
-        				answer+=1;
-        				break;
-        			}
+       			    break;
         		}
         	}
+        }
+        for(int i = 0; i < reserve.length; i++) {
+        	for(int j = 0; j < lost.length; j++) {
+        			if(lost[j]==reserve[i]-1 || lost[j]==reserve[i]+1 ) {
+        				if(answer >= n)
+        					break;
+        				System.out.println(reserve[i] + "가 " + lost[j]);
+        				reserve[i] = -1;
+        				answer+=1;
+        				break;
+        			}
+        	}
+        	
         }
         return answer;
     }
