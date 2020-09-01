fbfaf25c940f04b380f3c50e45c8c79b0deb8474
JeongaHan <ab012121@naver.com>
2020-09-01 17:21:58 +0900

    visualization sort process

--- src/datastructure/BubbleSort.java
+++ src/datastructure/BubbleSort.java
@@ -1,4 +1,5 @@
 package datastructure;
+
 import java.util.Scanner;
 
 public class BubbleSort {
@@ -8,34 +9,45 @@ public class BubbleSort {
 		a[idx1] = a[idx2];
 		a[idx2] = t;
 	}
-	
+
 	static void bubbleSort(int[] a, int n) {
-		for(int i = 0; i < n-1; i++) {
-			for(int j = 0; j < n-1-i; j++) {
-//				System.out.println(i + ", " + j);
-				if(a[j]>a[j+1]) {
-					swap(a,j,j+1);
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = n - 1; j > i; j--) {
+				for(int k = 0; k < j; k++) {
+					System.out.print( " " + a[k]);
 				}
+				if (a[j - 1] > a[j]) {
+					System.out.print("+" + a[j] +" ");
+					swap(a, j - 1, j);
+				}
+				else {
+					System.out.print("-" + a[j]+" ");
+				}
+				for(int k = j + 1 ; k <= n-1; k++) {
+					System.out.print(" " + a[k]);
+				}
+				System.out.println();
 			}
 		}
 	}
+
 	public static void main(String[] args) {
-		Scanner stdIn = new Scanner(System.in) ;
-		
+		Scanner stdIn = new Scanner(System.in);
+
 		System.out.println("버블정렬(버전1)");
 		System.out.print("요솟수 : ");
 		int nx = stdIn.nextInt();
 		int[] x = new int[nx];
-		
-		for(int i = 0; i < nx; i++) {
+
+		for (int i = 0; i < nx; i++) {
 			System.out.print("x[" + i + "] : ");
 			x[i] = stdIn.nextInt();
 		}
-		
+
 		bubbleSort(x, nx);
-		
+
 		System.out.println("오름차순으로 정렬했습니다.");
-		for(int i = 0; i < nx; i++) {
+		for (int i = 0; i < nx; i++) {
 			System.out.println("x[" + i + "]= " + x[i]);
 		}
 
