900e1b51ad796d28e1a58ec022df4d707a55221f
JeongaHan <44192694+JeongaHan@users.noreply.github.com>
2020-09-02 14:45:41 +0900

    3points operator

--- programmers/SecretMap.java
+++ programmers/SecretMap.java
@@ -6,16 +6,8 @@ class Solution {
 		
 		for (int i = 0; i < check1.length; i++) {
 			for (int j = 0; j < check1[i].length; j++) {
-				if (arr1[i] % 2 == 1) {
-					check1[i][j] = 1;
-				} else {
-					check1[i][j] = 0;
-				}
-				if (arr2[i] % 2 == 1) {
-					check2[i][j] = 1;
-				} else {
-					check2[i][j] = 0;
-				}
+				check1[i][j] = (arr1[i] % 2 == 1)? 1 : 0;
+				check2[i][j] = (arr2[i] % 2 == 1)? 1 : 0;
 				arr1[i] = arr1[i] / 2;
 				arr2[i] = arr2[i] / 2;
 			}
@@ -25,12 +17,7 @@ class Solution {
 		}
 		for(int i = check1.length-1; i >=0 ; i--) {
 			for(int j = check1.length-1; j >=0 ; j--) {
-				if(check1[i][j]==0 && check2[i][j]==0) {
-					answer[i] += " ";
-				}
-				else {
-					answer[i] += "#";
-				}
+				answer[i] += (check1[i][j]==0 && check2[i][j]==0)? " " : "#";
 			}
 		}
 		return answer;
